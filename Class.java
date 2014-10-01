210cacb6df98be8df0a718e6f5b50d2f1e1ac50f
Han Luo <luohan.da@hotmail.com>
2014-10-01 10:49:17 +0200

    class han

---
+++ Class.java
@@ -0,0 +1,23 @@
+
+public class Class {
+	
+	public void getAllAllowDevice(){
+		
+	}
+    public void DevicePin(){
+		
+	}
+	public void toggleDevice(){
+		
+	}
+	public void getHouseIP(){
+		
+	}
+	public void getHousePort(){
+		
+	}
+	public void addDeviceHistory(){
+		
+	}
+}
+
