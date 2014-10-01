f97ed9ed3e45aa9e3b17d93fd4aa5b1a723d3bfd
Han Luo <luohan.da@hotmail.com>
2014-10-01 11:28:18 +0200

    admin h

--- Admin.java
+++ Admin.java
@@ -1,8 +1,28 @@
 
-public class Admin {
+public class Admin {//h.l
 	
 	public void getAllUsers(){
 		
 	}
+	public void getAllPermissionsForUser(){
+		
+	}
+	public void getLogHistory(){
+		
+	}
+	public void getDeviceHistory(){
+		
+	}
+	public void changePermission(){
+		
+	}
+	public void createNewUser(){
+		
+	}
+	public void deleteUser(){
+		
+	}
+	
+	
 
 }
