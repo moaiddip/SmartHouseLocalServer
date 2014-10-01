df81c56809253baf5fe8e97c82e525963774ff0a
MathiasMatysiak <mathias1993@gmx.net>
2014-10-01 10:29:10 +0200

    Testing

--- LogIn.java
+++ LogIn.java
@@ -1,6 +1,6 @@
 public class LogIn {
 
-    private boolean userLogin =  false;    
+    private boolean userLogin =  false; //test   
     private String userID = "";    
     private String userType = "";    
 
