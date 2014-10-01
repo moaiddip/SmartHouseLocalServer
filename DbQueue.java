588a3f8fbebe5b0a2c0b4dbcf724e436daa4ac75
Tarik <Tarik@Tarik-Dator>
2014-10-01 15:20:10 +0200

    Change into DbQueue and User

--- DbQueue.java
+++ DbQueue.java
@@ -2,5 +2,21 @@ import java.sql.*;
 
 public class DbQueue {
 	
+	UserDb udb = null;
+	AdminDb adb = null;
 
+	public DbQueue(String userOrAdmin) {//
+
+		
+		if (userOrAdmin.equals("user")) {
+			udb = new UserDb();
+		} else if (userOrAdmin.equals("Admin")) {
+			adb = new AdminDb();
+		}
+
+	}
+
+	public DbQueue() {
+		// TODO Auto-generated constructor stub
+	}
 }
