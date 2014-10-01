8fb45685e7ec24a3be1bd8e7ac0e74538613b4d3
v4h4 <v4h4@v44h44>
2014-10-01 10:19:54 +0200

    Test changes

--- SocketListener.java
+++ SocketListener.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 public class SocketListener {
 	Socket socket;
 	int PORT=1111;
-	String SERVER="127.0.0.1";
+	String SERVER="127.0.0.1";//changes DIno
 	public static void main(String[] args) {
 		SocketListener sl = new SocketListener();
 		sl.socketConnections();//haha
