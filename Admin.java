4441c085b9c6ad2de035dff8bd9a4077b12b6075
Han Luo <luohan.da@hotmail.com>
2014-10-07 10:52:29 +0200

    admin

--- Admin.java
+++ Admin.java
@@ -1,25 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.*;
+
 
 public class Admin {
-	//Just testing 
-	//Tarik
-	public void getAllUsers(){
-		//Sepideh Test 
+    public static void main(String[] argv) throws Exception{
+    	//connect to DataBase;
+	    String driverName = "org.gjt.mm.mysql.Driver";
+	    Class.forName(driverName);
+	    String severName = "localhost";
+	    String mydatabase = "mydatabase";
+	    
+	    String url ="jdbcl://"+severName+ "/"+mydatabase;
+	    String adminname = "username";
+	    String password = "password";
+	    Connection connection = DriverManager.getConnection(url,adminname,password);
+	    
+	    User user = new User(null, password);
+    }
+    
+	public String getUsers(){
+		//create userlist;
+		ArrayList <String> list=new ArrayList<String>();
+		list.add("");
+		return getUsers();
 	}
-	public void getAllPermissionsForUser(){
-		
+	
+	public String getAllPermissionsForUser(){
+		return null;
 	}
-	public void getLogHistory(){
-		
+	
+	public String getLogHistory(){
+		return null;
 	}
-	public void getDeviceHistory(){
-		
+	
+	public String getDeviceHistory(){
+		return null;
 	}
+	
 	public void changePermission(){
 		
 	}
+	
 	public void createNewUser(){
-		
+		//create new user;
+		try{
+			System.out.println(User.getUsers());
 	}
+	
 	public void deleteUser(){
 		
 	}
@@ -27,3 +55,4 @@ public class Admin {
 	
 
 }
+
