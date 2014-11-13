bf08cc18090562bf6626a9e19bec715f9b9edaaf
ZijieFeng <jimeswals@gmail.com>
2014-11-13 13:10:55 +0100

    LogInDb_Zijie

--- LogInDb.java
+++ LogInDb.java
@@ -4,19 +4,14 @@ import javax.swing.JOptionPane;
 
 
 public class LogInDb {
-//	private String ipAdress = "localhost";
-//	private String port = "3306";
-//	private String url = "jdbc:mysql://" + ipAdress + ":" + port + "/";
-//	private String dbName = "LocalDatabase";
-//	private String driver = "com.mysql.jdbc.Driver";
-//	private String userName = "root";
-//	private String password = "7980066";
+
 	private Connection conn;
 	private Statement st;
 //	private ResultSet res;
 //	private PreparedStatement ps;
 //	private String user_ssn;	
 	private ResultSet rs;
+	private String query;
 	
 	
 	
@@ -41,6 +36,38 @@ public class LogInDb {
 		public boolean loginCheck(String username, String password){
 			String dbUsername, dbPassword;
 			boolean login=false;
+			
+			try {
+	            Class.forName("com.mysql.jdbc.Driver").newInstance();
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LocalDatabase", "root", "7980066");
+	            Statement stmt = (Statement) con.createStatement();
+	            query = "SELECT username, password FROM Users;";
+	            stmt.executeQuery(query);
+	            ResultSet rs = stmt.getResultSet();
+
+	            while(rs.next()){
+	                dbUsername = rs.getString("username");
+	                dbPassword = rs.getString("password");
+
+	                if(dbUsername == username && dbPassword == password){
+	                    System.out.println("OK");
+	                    login = true;
+	                }
+	                System.out.println(username + password + " " + dbUsername + dbPassword);
+	            }
+	        } catch (InstantiationException e) {
+	            e.printStackTrace();
+	        } catch (IllegalAccessException e) {
+	            e.printStackTrace();
+	        } catch (ClassNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return login;
+		}
+}
+	        /*
 			try{
 				while(rs.next()){
 					dbUsername=rs.getString("username");
@@ -50,14 +77,14 @@ public class LogInDb {
 						login = true;
 					}
 					System.out.println(username + password + "" +dbUsername + dbPassword);
-				return true;
+				return false;
 				}
 			}
 				
 				catch(Exception e){
 				e.printStackTrace();
 			   }
-			return false;
+			return login;
 		}
 
 
