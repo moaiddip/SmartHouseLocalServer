8c906b6eedafa3e0d338ff3b74747901938cbd23
MathiasMatysiak <mathias1993@gmx.net>
2014-11-19 15:48:48 +0100

    Applied naming convention on UserDB Applied security measures

--- UserDb.java
+++ UserDb.java
@@ -26,7 +26,7 @@ public class UserDb {
 	private ResultSet res;
 	private PreparedStatement ps;
 	private String user_ssn;
-	private boolean printMsg;
+	private boolean printMsg = false;
 
 	public UserDb() {
 		connect();
@@ -37,8 +37,7 @@ public class UserDb {
 
 	private void testConnection() {
 		try {
-			System.out
-					.println(isAuthorized("820907-9002", "S3P1D1H"));
+			// System.out.println(isAuthorized("820907-9002", "S3P1D1H"));
 			// toggleDevice(3,true);
 			// addDeviceHistory(3,true);
 			// getDevicePin(3);
@@ -56,14 +55,18 @@ public class UserDb {
 	}
 
 	public void connect() {
-		System.out.print("Trying to connect to database . . . ");
+		if (printMsg) {
+			System.out.print("Trying to connect to database . . . ");
+		}
 		try {
 
 			Class.forName(driver).newInstance();
 			conn = DriverManager
 					.getConnection(url + dbName, userName, password);
 			st = conn.createStatement();
-			System.out.println("Connected!!!");
+			if (printMsg) {
+				System.out.println("Connected!!!");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -81,14 +84,18 @@ public class UserDb {
 		}
 	}
 
-	public ArrayList toggleDevice(int deviceID, boolean state) {
+	/*
+	 * QUERY + UPDATE
+	 * created by Mathias
+	 */
+	public ArrayList toggleDevice(int deviceID, int state) {
 		ArrayList toggleList = new ArrayList();
-		String query = "SELECT isAllowed\n" 
-				+ "FROM Permissions \n"
-				+ "WHERE userSSN = '879724-6009' \n" 
-				+ "AND deviceId = " + deviceID + ";";
-		String update = "UPDATE Devices SET deviceState= ? "
-				+ " WHERE deviceId = ? ;";
+		String query = "SELECT permission.isAllowed\n" //
+				+ "FROM permission \n" //
+				+ "WHERE permission.userSSN = '879724-6009' \n" // Add userId
+				+ "AND permission.id = " + deviceID + ";";//
+		String update = "UPDATE device SET device.state= ? "
+				+ " WHERE device.id = ? ;";
 		if (printMsg) {
 			System.out.println("******QUERY****** \n" + query
 					+ "\n******************");
@@ -101,38 +108,44 @@ public class UserDb {
 			boolean isAllowed = res.getBoolean("isAllowed");
 			if (isAllowed == true) {
 				ps = conn.prepareStatement(update);
-				ps.setBoolean(1, state);
+				ps.setInt(1, state);
 				ps.setInt(2, deviceID);
 				ps.executeUpdate();
-				res = st.executeQuery("SELECT devicename, DeviceState \n"
-						+ "FROM Devices "
-						+ "WHERE deviceId = " + deviceID + ";");
+				res = st.executeQuery("SELECT device.name, device.state \n"
+						+ "FROM device " + "WHERE device.id = " + deviceID
+						+ ";");
 				res.next();
-				toggleList.add(res.getString("deviceName"));
-				toggleList.add(res.getBoolean("deviceState"));
-				addDeviceHistory(deviceID, state);
+				toggleList.add(res.getString("device.name"));
+				toggleList.add(res.getInt("device.state"));
+				insertDeviceHistory(deviceID, state);
 			}
 			if (printMsg) {
 				System.out.println("Update Succesful");
 			}
 		} catch (Exception e) {
 			System.out.print(e);
-		} 
+		}
 		return toggleList;
 	}
 
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
 	public String getDeviceAbr(int deviceID) {
-		String query = "SELECT deviceAbreviation \n" + "FROM Devices \n"
-				+ "WHERE deviceID = " + deviceID + ";";
-		System.out.println("******UPDATE****** \n" + query
-				+ "\n*****************");
+		String query = "SELECT device.abbreviation \n" + "FROM device \n"
+				+ "WHERE device.id = " + deviceID + ";";
+		if (printMsg) {
+			System.out.println("******UPDATE****** \n" + query
+					+ "\n*****************");
+		}
 		try {
 			res = st.executeQuery(query);// //////////
 			res.next();
 			if (printMsg) {
 				System.out.println("Query Succesful");
 			}
-			return res.getString("deviceAbreviation");
+			return res.getString("device.abbreviation");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.print(e);
@@ -141,13 +154,43 @@ public class UserDb {
 
 	}
 
-	private void addDeviceHistory(int deviceID, boolean state) {
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
+	public ArrayList getRooms() {
+		ArrayList roomInfo = new ArrayList<String>();
+		String query = "SELECT room.id, room.name, room.imagePath \n"//
+				+ "FROM room;";
+		if (printMsg) {
+			System.out.println("******UPDATE****** \n" + query
+					+ "\n*****************");
+		}
+		try {
+			res = st.executeQuery(query);
+			if (printMsg) {
+				System.out.println("Query Succesful");
+			}
+			while (res.next()) {
+				roomInfo.add(String.valueOf(res.getInt("room.id")));
+				roomInfo.add(res.getString("room.name"));
+				roomInfo.add(res.getString("room.imagePath"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.print(e);
+		}
+		return roomInfo;
 
 	}
 
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
 	public int getDevicePin(int deviceID) {
-		String query = "SELECT devicePin \n" + "FROM Devices \n"
-				+ "WHERE deviceID = " + deviceID + ";";
+		String query = "SELECT device.pin \n" + "FROM device \n"
+				+ "WHERE device.id = " + deviceID + ";";
 		if (printMsg) {
 			System.out.println("******UPDATE****** \n" + query
 					+ "\n*****************");
@@ -158,7 +201,7 @@ public class UserDb {
 			if (printMsg) {
 				System.out.println("Query Succesful");
 			}
-			return res.getInt("devicePin");
+			return res.getInt("device.pin");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.print(e);
@@ -166,98 +209,76 @@ public class UserDb {
 		return -1;
 	}
 
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
 	public void getAllAllowedDevices() {
-		String query = "SELECT devices.deviceName, rooms.roomName, deviceState\n"
-				+ "FROM rooms, devices, permissions\n"
-				+ "WHERE rooms.roomId = devices.roomId\n"
-				+ "AND devices.deviceId = permissions.deviceId\n"
-				+ "AND permissions.UserSSN = '9310101337'\n" // later User.ssn
-				+ "AND permissions.isAllowed = true" + ";";
+		String query = "SELECT device.name, room.name, device.state\n"
+				+ "FROM room, device, permission\n"
+				+ "WHERE room.id = device.id\n"
+				+ "AND device.id = permission.id\n"
+				+ "AND permission.userSSN = '9310101337'\n" // later User.ssn
+				+ "AND permission.isAllowed = true" + ";";
 		if (printMsg) {
 			System.out.println("******UPDATE****** \n" + query
 					+ "\n*****************");
 		}
 		try {
-			res = st.executeQuery(query);// //////////
-			System.out.println("Query Succesful");
-			while (res.next()) {// /////////
-				System.out.println(res.getString("devices.deviceName") + " "
-						+ res.getString("rooms.roomName") + " "
-						+ res.getBoolean("devices.deviceState"));// ////////
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.print(e);
-		} finally {
-			disconnect();
-		}
-	}
-
-	public ArrayList checkDevice(int deviceID) {
-		
-		ArrayList checkList = new ArrayList();
-		try {
-			res = st.executeQuery("SELECT deviceName,deviceState FROM Devices WHERE deviceId="
-					+ deviceID + ";");
-			res.next();
-			checkList.add(res.getString("Devices.deviceName"));
-			checkList.add(res.getBoolean("Devices.deviceState"));
-			System.out.println("Update Succesful");
-		} catch (Exception e) {
-			System.out.print(e);
-		} 
-		return checkList;
-	}
-
-	public ArrayList checkAllDevices() {
-		ArrayList checkAllList = new ArrayList();
-		try {
-			checkAllList.add("checkAllDevices");
-			res = st.executeQuery("SELECT deviceName, deviceState FROM Devices WHERE deviceId>0");
-			while (res.next()) {
-				checkAllList.add(res.getString("deviceName"));
-				checkAllList
-						.add(Boolean.toString(res.getBoolean("deviceState")));
-			}
-			if (printMsg) {
-				System.out.println("Update Succesful");
-			}
-		} catch (Exception e) {
-			System.out.print(e);
-			checkAllList = null;
-		}
-		return checkAllList;
-	}
-
-	public ArrayList testDevice(int deviceID) {
-		ArrayList<String> returnArray = new ArrayList<String>();
-		String query = "SELECT deviceName, deviceState\n" + "FROM Devices\n"
-				+ "WHERE Devices.deviceId = " + deviceID + ";";
-		String updateOn = "UPDATE devices\n" + "SET deviceState = true " + "\n"
-				+ "WHERE deviceId = " + deviceID;
-		String updateOff = "UPDATE devices\n" + "SET deviceState = false "
-				+ "\n" + "WHERE deviceId = " + deviceID;
-		try {
-
 			res = st.executeQuery(query);
-			res.next();
-			// boolean isAllowed = res.getBoolean("isAllowed");
-			String s = res.getString("Devices.deviceName");
-			returnArray.add(s);
-			st.executeUpdate(updateOn);
-			Thread.sleep(100);
-			st.executeUpdate(updateOff);
-			returnArray.add("true");
+			if (printMsg) {
+				System.out.println("Query Succesful");
+			}
+			while (res.next()) {
+				System.out.println(res.getString("device.name") + " "
+						+ res.getString("room.name") + " "
+						+ res.getBoolean("device.state"));
+			}
 		} catch (Exception e) {
-			returnArray.add("false");
 			e.printStackTrace();
+			System.out.print(e);
 		}
-		return returnArray;
 	}
 
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
+	public void getAllAllowedDevicesByRoom(int roomId) {
+		String query = "SELECT device.name, room.name, device.state\n" //
+				+ "FROM room, device, permission\n" //
+				+ "WHERE room.id = device.id\n"//
+				+ "AND roomId = " + roomId + "\n" //
+				+ "AND device.id = permission.id\n" //
+				+ "AND permission.userSSN = '9310101337'\n" // later User.ssn
+				+ "AND permission.isAllowed = true" + ";";
+		if (printMsg) {
+			System.out.println("******UPDATE****** \n" + query
+					+ "\n*****************");
+		}
+		try {
+			res = st.executeQuery(query);
+			if (printMsg) {
+				System.out.println("Query Succesful");
+			}
+			while (res.next()) {
+				System.out.println(res.getString("device.name") + " "
+						+ res.getString("room.name") + " "
+						+ res.getBoolean("device.state"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.print(e);
+		}
+	}
+
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
 	public boolean isAuthorized(String userSSN, String password) {
-		String query = "SELECT userSSN, userPassword\n" + "From users\n"
-				+ "Where userSSN = '" + userSSN + "' and userPassword = '"
+		String query = "SELECT user.SSN, user.password\n" + "FROM user\n"
+				+ "WHERE user.SSN = '" + userSSN + "' AND user.password = '"
 				+ password + "';";
 		if (printMsg) {
 			System.out.println("******UPDATE****** \n" + query
@@ -269,7 +290,9 @@ public class UserDb {
 			while (res.next()) {
 				s = res.getString("userSSN");
 			}
-			System.out.println("Query Succesful");
+			if (printMsg) {
+				System.out.println("Query Succesful");
+			}
 			if (s != "") {
 				return true;
 			}
@@ -279,48 +302,14 @@ public class UserDb {
 		return false;
 	}
 
-	public void setSessionKey(String byteArray) {
-		
-		String update = "UPDATE users\n" + "SET userSessionKey = " + byteArray
-				+ "\n" + "WHERE userSSN = '" + user_ssn + "';";
-		if (printMsg) {
-			System.out.println("******UPDATE****** \n" + update
-					+ "\n*****************");
-		}
-		try {
-			st.executeUpdate(update);
-			System.out.println("Update Succesful");
-		} catch (Exception e) {
-			System.out.print(e);
-		}
-		}
-
-	public ArrayList getSessionKey() {
-		
-		ArrayList<String> returnArray = new ArrayList<String>();
-		String query = "SELECT userSessionKey\n" + "From users\n"
-				+ "Where userSSN = '" + user_ssn + "';";
-		if (printMsg) {
-			System.out.println("******QUERY****** \n" + query
-					+ "\n*****************");
-		}
-		try {
-			res = st.executeQuery(query);
-			while (res.next()) {
-				returnArray.add(res.getString("userSessionKey"));
-			}
-			System.out.println("Query Succesful");
-		} catch (Exception e) {
-			System.out.print(e);
-		}
-		return returnArray;
-	}
-
+	/*
+	 * QUERY
+	 * created by Mathias
+	 */
 	public ArrayList getPriority(String command) {
-		
 		ArrayList<Integer> returnArray = new ArrayList<Integer>();
-		String query = "SELECT commandPriority\n" + "From commands\n"
-				+ "Where commandName = '" + command + "';";
+		String query = "SELECT command.priority\n" + "FROM command\n"
+				+ "WHERE command.name = '" + command + "';";
 		if (printMsg) {
 			System.out.println("******QUERY****** \n" + query
 					+ "\n*****************");
@@ -328,17 +317,23 @@ public class UserDb {
 		try {
 			res = st.executeQuery(query);
 			while (res.next()) {
-				returnArray.add(res.getInt("commandPriority"));
+				returnArray.add(res.getInt("command.priority"));
 			}
-			System.out.println("Query Succesful");
+			if (printMsg) {
+				System.out.println("Query Succesful");
+			}
 		} catch (Exception e) {
 			System.out.print(e);
 		}
 		return returnArray;
 	}
 
+	/*
+	 * INSERT
+	 * created by Mathias
+	 */
 	public void insertLogInHistory(String state) {
-		String insert = "INSERT INTO LogHistory(logHistoryId,userSSN,state, ip, timestamp)\n"
+		String insert = "INSERT INTO logHistory(id,userSSN,state, ip, timestamp)\n"
 				+ "VALUES( null , ? , ? , ? , null);";
 		if (printMsg) {
 			System.out.println("******INSERT****** \n" + insert
@@ -358,8 +353,12 @@ public class UserDb {
 		}
 	}
 
-	public void insertDeviceHistory(int deviceId, String state) {
-		String insert = "INSERT INTO DeviceHistory(deviceHistoryId,userSSN, deviceId ,state,  timestamp)\n"
+	/*
+	 * INSERT
+	 * created by Mathias
+	 */
+	public void insertDeviceHistory(int deviceId, int state) {
+		String insert = "INSERT INTO deviceHistory(id,userSSN, deviceId ,state,  timestamp)\n"
 				+ "VALUES( null , ? , ? , ? , null);";
 		if (printMsg) {
 			System.out.println("******INSERT****** \n" + insert
@@ -369,7 +368,7 @@ public class UserDb {
 			PreparedStatement ps = conn.prepareStatement(insert);
 			ps.setString(1, user_ssn);
 			ps.setInt(2, deviceId);
-			ps.setString(3, state);
+			ps.setInt(3, state);
 			if (printMsg) {
 				System.out.println("Insert Succesful");
 			}
@@ -390,42 +389,159 @@ public class UserDb {
 			while (res.next()) {
 				System.out.println(res.getString("roomName"));
 			}
-			System.out.println("Query Succesful");
+			if (printMsg) {
+				System.out.println("Query Succesful");
+			}
 		} catch (Exception e) {
 			System.out.print(e);
 		}
 	}
 
 	public void defaultUpdate() {
-		String update = "UPDATE devices\n" 
-				+ "SET deviceState = ? \n"
-				+ "WHERE deviceId = ?";
+		String update = "UPDATE device\n" + "SET device.state = ? \n"
+				+ "WHERE device.id = ?";
 		try {
-			
+
 			ps = conn.prepareStatement(update);
-			ps.setBoolean(1, true);
+			ps.setInt(1, 1);
 			ps.setInt(2, 32);
-			if (printMsg) {System.out.println("******UPDATE****** \n" + update + "\n*****************");}
+			if (printMsg) {
+				System.out.println("******UPDATE****** \n" + update
+						+ "\n*****************");
+			}
 			ps.executeUpdate();
-			if (printMsg) {System.out.println("Update Succesful");}
+			if (printMsg) {
+				System.out.println("Update Succesful");
+			}
 		} catch (Exception e) {
 			System.out.print(e);
-		} 
+		}
 	}
 
 	public void defaultInsert() {
 		String insert = "INSERT INTO course(tablename1,tablename2,tablename3)\n"
 				+ "VALUES( ? , ? , ? );";
-		if (printMsg) {System.out.println("******INSERT****** \n" + insert + "\n*****************");}
+		if (printMsg) {
+			System.out.println("******INSERT****** \n" + insert
+					+ "\n*****************");
+		}
 		try {
 			ps = conn.prepareStatement(insert);
 			ps.setString(1, "Test");
 			ps.setString(2, "Test2");
 			ps.setString(3, "Test3");
 			ps.executeUpdate();
-			if (printMsg) {System.out.println("Insert Succesful");}
+			if (printMsg) {
+				System.out.println("Insert Succesful");
+			}
 		} catch (Exception e) {
 			System.out.print(e);
 		}
 	}
 }
+
+// public ArrayList checkDevice(int deviceID) {
+//
+// ArrayList checkList = new ArrayList();
+// try {
+// res =
+// st.executeQuery("SELECT device.name,device.state FROM device WHERE device.id="
+// + deviceID + ";");
+// res.next();
+// checkList.add(res.getString("device.name"));
+// checkList.add(res.getBoolean("device.state"));
+// if (printMsg) {
+// System.out.println("Update Succesful");
+// }
+// } catch (Exception e) {
+// System.out.print(e);
+// }
+// return checkList;
+// }
+
+// public ArrayList checkAllDevices() {
+// ArrayList checkAllList = new ArrayList();
+// try {
+// checkAllList.add("checkAllDevices");
+// res =
+// st.executeQuery("SELECT device.name, device.state FROM device WHERE device.id>0");
+// while (res.next()) {
+// checkAllList.add(res.getString("device.name"));
+// checkAllList.add(Boolean.toString(res
+// .getBoolean("device.state")));
+// }
+// if (printMsg) {
+// System.out.println("Update Succesful");
+// }
+// } catch (Exception e) {
+// System.out.print(e);
+// checkAllList = null;
+// }
+// return checkAllList;
+// }
+
+// public ArrayList testDevice(int deviceID) {
+// ArrayList<String> returnArray = new ArrayList<String>();
+// String query = "SELECT device.name, device.state\n" + "FROM device\n"
+// + "WHERE device.id = " + deviceID + ";";
+// String updateOn = "UPDATE device\n" + "SET device.state = true " + "\n"
+// + "WHERE device.id = " + deviceID;
+// String updateOff = "UPDATE device\n" + "SET device.state = false "
+// + "\n" + "WHERE device.id = " + deviceID;
+// try {
+//
+// res = st.executeQuery(query);
+// res.next();
+// // boolean isAllowed = res.getBoolean("isAllowed");
+// String s = res.getString("device.name");
+// returnArray.add(s);
+// st.executeUpdate(updateOn);
+// Thread.sleep(100);
+// st.executeUpdate(updateOff);
+// returnArray.add("true");
+// } catch (Exception e) {
+// returnArray.add("false");
+// e.printStackTrace();
+// }
+// return returnArray;
+// }
+// public void setSessionKey(String byteArray) {
+//
+// String update = "UPDATE user\n" + "SET user.sessionKey = " + byteArray
+// + "\n" + "WHERE user.SSN = '" + user_ssn + "';";
+// if (printMsg) {
+// System.out.println("******UPDATE****** \n" + update
+// + "\n*****************");
+// }
+// try {
+// st.executeUpdate(update);
+// if (printMsg) {
+// System.out.println("Update Succesful");
+// }
+// } catch (Exception e) {
+// System.out.print(e);
+// }
+// }
+//
+// public ArrayList getSessionKey() {
+//
+// ArrayList<String> returnArray = new ArrayList<String>();
+// String query = "SELECT user.sessionKey\n" + "From user\n"
+// + "Where user.SSN = '" + user_ssn + "';";
+// if (printMsg) {
+// System.out.println("******QUERY****** \n" + query
+// + "\n*****************");
+// }
+// try {
+// res = st.executeQuery(query);
+// while (res.next()) {
+// returnArray.add(res.getString("user.sessionKey"));
+// }
+// if (printMsg) {
+// System.out.println("Query Succesful");
+// }
+// } catch (Exception e) {
+// System.out.print(e);
+// }
+// return returnArray;
+// }
